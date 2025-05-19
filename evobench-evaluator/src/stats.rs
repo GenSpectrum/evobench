198d2c963b913880f4065e25077566aab18eac76
Christian Jaeger <ch@christianjaeger.ch>
2025-05-19 12:37:26 +0200

    stats: add tests, fixes
    - add "a virtual amount of" 0.5 to average for rounding up, adding
      StatsError::SaturatedU128 for the unlikely case that it overflows
    
    - messing around with bucketing algorithm, not right yet.

--- evobench-evaluator/src/stats.rs
+++ evobench-evaluator/src/stats.rs
@@ -44,10 +44,11 @@ pub struct Stats<ViewType, const TILES_COUNT: usize> {
     view_type: PhantomData<fn() -> ViewType>,
     pub num_values: usize,
     pub sum: u128,
-    pub average: u64, // rounded down
-    /// Percentiles or in whatever number of sections you asked:
-    /// sample count is the index, the sample value there is the value
-    /// in the vector.
+    pub average: u64, // rounded to nearest
+    /// Percentiles or in `TILES_COUNT` number of sections. Sample
+    /// count is the index, the sample value there is the value in the
+    /// vector. `tiles[0]` is the mininum, `tiles[TILES_COUNT]` the
+    /// maximum sample value.
     pub tiles: Vec<u64>,
 }
 
@@ -55,25 +56,33 @@ pub struct Stats<ViewType, const TILES_COUNT: usize> {
 pub enum StatsError {
     #[error("no inputs given")]
     NoInputs,
+    #[error("u128 saturated -- u64::MAX values summing up to near u128::MAX")]
+    SaturatedU128,
 }
 
 impl<ViewType, const TILES_COUNT: usize> Stats<ViewType, TILES_COUNT> {
     /// `tiles_count` is how many 'tiles' to build, for percentiles
-    /// give the number 101.
+    /// give the number 101. (Needs to own `vals` for sorting,
+    /// internally.)
     pub fn from_values(mut vals: Vec<u64>) -> Result<Self, StatsError> {
         let num_values = vals.len();
         if num_values.is_zero() {
             return Err(StatsError::NoInputs);
         }
         let sum: u128 = vals.iter().map(|v| u128::from(*v)).sum();
-        let average = sum / (num_values as u128);
+        let average = {
+            let num_values = num_values as u128;
+            sum.checked_add(num_values / 2)
+                .ok_or(StatsError::SaturatedU128)?
+                / num_values
+        };
         vals.sort();
 
-        let flen = (num_values - 1) as f64;
+        let flen = num_values as f64;
         let mut tiles = Vec::new();
         let tiles_max = TILES_COUNT as f64;
         for i in 0..TILES_COUNT {
-            let index = i as f64 / tiles_max * flen;
+            let index = i as f64 / tiles_max * flen + 0.5;
             let val = vals[index as usize];
             tiles.push(val);
         }
@@ -85,7 +94,7 @@ impl<ViewType, const TILES_COUNT: usize> Stats<ViewType, TILES_COUNT> {
             view_type: PhantomData::default(),
             num_values,
             sum,
-            average: average.try_into().expect("always fit"),
+            average: average.try_into().expect("always fits"),
             tiles,
         })
     }
@@ -170,5 +179,44 @@ impl<ViewType: From<u64> + Display, const TILES_COUNT: usize> Display
             ViewType::from(*average),
             ViewType::from(self.median())
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Result;
+
+    use super::*;
+
+    #[test]
+    fn t_average_and_tiles() -> Result<()> {
+        let data = vec![23, 4, 8, 30, 7];
+        let stats = Stats::<u64, 4>::from_values(data)?;
+        assert_eq!(stats.average, 14); // 14.4
+        assert_eq!(stats.tiles, [4, 7, 23, 30]); // 8 skipped
+
+        let data = vec![23, 4, 8, 31, 7];
+        let stats = Stats::<u64, 4>::from_values(data)?;
+        assert_eq!(stats.average, 15); // 14.6
+        assert_eq!(stats.tiles[0], 4);
+        assert_eq!(stats.tiles.len(), 4);
+        assert_eq!(stats.tiles[3], 31);
+        assert_eq!(stats.tiles, [4, 7, 23, 31]); // 8 skipped
+
+        let data = vec![23, 4, 8, 7];
+        let stats = Stats::<u64, 4>::from_values(data)?;
+        assert_eq!(stats.average, 11); // 10.5
+        assert_eq!(stats.tiles, [4, 7, 8, 23]);
+
+        let data = vec![23, 8, 7];
+        let stats = Stats::<u64, 4>::from_values(data)?;
+        assert_eq!(stats.average, 13); // 12.6666666666667
+        assert_eq!(stats.tiles, [7, 8, 23, 23]);
+        // interesting that it duplicates 23 not 8; `let index = i as
+        // f64 / tiles_max * flen + 0.499;` above leads to 8 being
+        // duplicated, but then skips 23 in the test with 5 values
+        // above.
+
+        Ok(())
     }
 }
