fb0d2f112b87d460fcf9fc622d4d70541b0ffec3
Christian Jaeger <ch@christianjaeger.ch>
2025-05-26 17:52:54 +0200

    style: add clang-format exceptions, run clang-format

--- evobench-probes/examples/sum_of_fibs.cpp
+++ evobench-probes/examples/sum_of_fibs.cpp
@@ -1,43 +1,43 @@
-#include <iostream>
-#include <unistd.h>
 #include <time.h>
+#include <unistd.h>
+#include <iostream>
 
 #include "../include/evobench/evobench.hpp"
 
 long long fib(long long n) {
-    // EVOBENCH_SCOPE("fib", "fib");
-    if (n <= 2) {
-        return n;
-    }
-    return fib(n - 1) + fib(n - 2);
+   // EVOBENCH_SCOPE("fib", "fib");
+   if (n <= 2) {
+      return n;
+   }
+   return fib(n - 1) + fib(n - 2);
 }
 
 long long sum_of_fibs(long long n) {
-    {
-        char buf[30];
-        snprintf(buf, 30, "%lld", n);
-        EVOBENCH_KEY_VALUE("sum_of_fibs n", buf);
-    }
+   {
+      char buf[30];
+      snprintf(buf, 30, "%lld", n);
+      EVOBENCH_KEY_VALUE("sum_of_fibs n", buf);
+   }
 
-    EVOBENCH_SCOPE("main", "sum_of_fibs");
-    long long z = 0;
-    for (long long i = 0; i < n; i++) {
-        EVOBENCH_SCOPE("main", "fib");
-        struct timespec req = {
-            .tv_sec = 0,
-            .tv_nsec = 10000000,
-        };
-        nanosleep(&req, NULL);
-        z += fib(i);
-    }
-    return z;
+   EVOBENCH_SCOPE("main", "sum_of_fibs");
+   long long z = 0;
+   for (long long i = 0; i < n; i++) {
+      EVOBENCH_SCOPE("main", "fib");
+      struct timespec req = {
+         .tv_sec = 0,
+         .tv_nsec = 10000000,
+      };
+      nanosleep(&req, NULL);
+      z += fib(i);
+   }
+   return z;
 }
 
 int main() {
-    EVOBENCH_SCOPE("main", "main");
-    sleep(1);
-    for (long long i = 0; i < 40; i++) {
-        std::cout << "sum_of_fibs(" << i << ") = " << sum_of_fibs(i) << "\n";
-    }
-    return 0;
+   EVOBENCH_SCOPE("main", "main");
+   sleep(1);
+   for (long long i = 0; i < 40; i++) {
+      std::cout << "sum_of_fibs(" << i << ") = " << sum_of_fibs(i) << "\n";
+   }
+   return 0;
 }
