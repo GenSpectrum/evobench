84c4942ef884dd3b83a431e59804d3c216dd05fb
Christian Jaeger <ch@christianjaeger.ch>
2025-06-10 17:17:07 +0200

    zstd_file.rs decompressed_file: verify second file extension

--- evobench-evaluator/src/zstd_file.rs
+++ evobench-evaluator/src/zstd_file.rs
@@ -11,39 +11,106 @@ use ruzstd::StreamingDecoder;
 
 const USING_EXTERNAL_TOOL: bool = false;
 
-/// Transparently decompress zstd files if they have a .zstd suffix;
-/// after that, expecting the `expected_suffix` (XX well, currently
-/// not checking the sub-suffix if it has .zstd suffix).
-pub fn decompressed_file(path: &Path, expected_suffix: &str) -> Result<Box<dyn Read>> {
+#[derive(Debug, PartialEq)]
+pub enum Extension<'s> {
+    ZStd,
+    Other(&'s str),
+}
+
+pub fn file_extension<'s, P: AsRef<Path>>(
+    path: P,
+    expected_suffix: &'s str,
+) -> Result<Extension<'s>> {
+    let path = path.as_ref();
     let ext = path.extension().ok_or_else(|| {
-        anyhow!("missing file extension, expecting {expected_suffix:?} or \".zstd\": {path:?}")
+        anyhow!("missing file extension, expecting {expected_suffix:?} or \"zstd\": {path:?}")
     })?;
 
-    let is_compressed = match ext.to_string_lossy().as_ref() {
-        "zstd" => true,
-        s if &*s == expected_suffix => false,
-        _ => bail!("unknown file extension {ext:?}, expecting .log or .zstd: {path:?}"),
+    match ext.to_string_lossy().as_ref() {
+        "zstd" => {
+            let stem = path.with_extension("");
+            let ext2 = stem.extension().ok_or_else(|| {
+                anyhow!(
+                    "missing second file extension, after \"zstd\", \
+                     expecting {expected_suffix:?}: {path:?}"
+                )
+            })?;
+            match ext2.to_string_lossy().as_ref() {
+                s if &*s == expected_suffix => Ok(Extension::ZStd),
+                _ => bail!(
+                    "unknown second file extension {ext2:?} after \"zstd\", \
+                     expecting {expected_suffix:?}: {path:?}"
+                ),
+            }
+        }
+        s if &*s == expected_suffix => Ok(Extension::Other(expected_suffix)),
+        _ => bail!(
+            "unknown file extension {ext:?}, expecting {expected_suffix:?} \
+             or \"zstd\": {path:?}"
+        ),
+    }
+}
+
+#[test]
+fn t_file_extension() {
+    use Extension::*;
+    let ok = |a: &str, b: &'static str| {
+        file_extension(a, b).expect("test call should not give an error")
     };
+    let err = |a: &str, b: &'static str| {
+        file_extension(a, b)
+            .err()
+            .expect("test call should give an error")
+            .to_string()
+    };
+    assert_eq!(ok("foo.x", "x"), Other("x"));
+    assert_eq!(ok("foo.x.zstd", "x"), ZStd);
+    assert_eq!(ok("foo.z.x", "x"), Other("x"));
+    assert_eq!(ok("foo.z.x.zstd", "x"), ZStd);
+    assert_eq!(
+        err("foo.x", "y"),
+        "unknown file extension \"x\", expecting \"y\" or \"zstd\": \"foo.x\""
+    );
+    assert_eq!(
+        err("foo.x.zstd", "y"),
+        "unknown second file extension \"x\" after \"zstd\", expecting \"y\": \"foo.x.zstd\""
+    );
+    assert_eq!(
+        err("foo.zstd", "y"),
+        "missing second file extension, after \"zstd\", expecting \"y\": \"foo.zstd\""
+    );
+    assert_eq!(
+        err("foo", "y"),
+        "missing file extension, expecting \"y\" or \"zstd\": \"foo\""
+    );
+}
+
+/// Transparently decompress zstd files if they have a .zstd suffix;
+/// after that, expecting the `expected_suffix`
+pub fn decompressed_file(path: &Path, expected_suffix: &str) -> Result<Box<dyn Read>> {
+    let ext = file_extension(path, expected_suffix)?;
 
     let file_open = || File::open(path).with_context(|| anyhow!("opening file {path:?}"));
 
-    if is_compressed {
-        if USING_EXTERNAL_TOOL {
-            let mut c = Command::new("zstd");
-            let args: Vec<OsString> = vec!["-dcf".into(), "--".into(), path.into()];
-            c.args(args);
-            c.stdout(Stdio::piped());
-            let child = c
-                .spawn()
-                .with_context(|| anyhow!("opening file {path:?}"))?;
-            Ok(Box::new(child.stdout.expect("present since configured")))
-        } else {
-            let input = file_open()?;
-            Ok(Box::new(
-                StreamingDecoder::new(input).with_context(|| anyhow!("zstd-decoding {path:?}"))?,
-            ))
+    match ext {
+        Extension::ZStd => {
+            if USING_EXTERNAL_TOOL {
+                let mut c = Command::new("zstd");
+                let args: Vec<OsString> = vec!["-dcf".into(), "--".into(), path.into()];
+                c.args(args);
+                c.stdout(Stdio::piped());
+                let child = c
+                    .spawn()
+                    .with_context(|| anyhow!("opening file {path:?}"))?;
+                Ok(Box::new(child.stdout.expect("present since configured")))
+            } else {
+                let input = file_open()?;
+                Ok(Box::new(
+                    StreamingDecoder::new(input)
+                        .with_context(|| anyhow!("zstd-decoding {path:?}"))?,
+                ))
+            }
         }
-    } else {
-        Ok(Box::new(file_open()?))
+        Extension::Other(_) => Ok(Box::new(file_open()?)),
     }
 }
