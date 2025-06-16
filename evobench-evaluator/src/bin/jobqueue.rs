9fd0ad621328a11a984aaa0700d54d05af6a899a
Christian Jaeger <ch@christianjaeger.ch>
2025-06-17 00:30:57 +0200

    Revert "jobqueue: ask clap for OsString, still doesn't allow non-utf8 data"
    This reverts commit a38cc0ef36b660a63fccf17eebe90c3c3721ed16.

--- evobench-evaluator/src/bin/jobqueue.rs
+++ evobench-evaluator/src/bin/jobqueue.rs
@@ -62,7 +62,7 @@ enum SubCommand {
     Insert {
         /// The arguments to be passed to the program when using the
         /// `run` subcommand
-        arguments: Vec<OsString>,
+        arguments: Vec<SafeString>,
     },
     /// Process the entries in the queue
     Run {
@@ -100,7 +100,7 @@ enum SubCommand {
         /// Program name or path to run
         program: PathBuf,
         /// Program arguments to be prepended to the ones from the queue
-        first_arguments: Vec<OsString>,
+        first_arguments: Vec<SafeString>,
     },
 }
 
@@ -123,10 +123,6 @@ where
         Ok(exit_code)
     })()
     .with_context(|| anyhow!("running {:?}", command.as_ref()))
-}
-
-fn safestrings_from_osstrings(arguments: Vec<OsString>) -> Vec<SafeString> {
-    arguments.into_iter().map(|v| v.into()).collect()
 }
 
 fn main() -> Result<()> {
@@ -177,9 +173,7 @@ fn main() -> Result<()> {
                 println!("{file_name} ({key})\t{locking}\t{val:?}");
             }
         }
-        SubCommand::Insert { arguments } => {
-            queue.push_front(&safestrings_from_osstrings(arguments))?
-        }
+        SubCommand::Insert { arguments } => queue.push_front(&arguments)?,
         SubCommand::Run {
             no_lock,
             limit,
@@ -190,7 +184,6 @@ fn main() -> Result<()> {
             program,
             first_arguments,
         } => {
-            let first_arguments = safestrings_from_osstrings(first_arguments);
             let opts = QueueIterationOpts {
                 no_lock,
                 error_when_locked,
