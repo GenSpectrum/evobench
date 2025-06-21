e43d54b611ed9249e9a3b564968749f210617d90
Christian Jaeger <ch@christianjaeger.ch>
2025-06-21 12:33:00 +0200

    add serde/git_url.rs (mostly copy of ProperFilename)

---
+++ evobench-evaluator/src/serde/git_url.rs
@@ -0,0 +1,120 @@
+use std::{fmt::Display, str::FromStr};
+
+use serde::de::Visitor;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, serde::Serialize)]
+pub struct GitUrl(String);
+
+impl GitUrl {
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+impl AsRef<str> for GitUrl {
+    fn as_ref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl<'t> From<&'t GitUrl> for &'t str {
+    fn from(value: &'t GitUrl) -> Self {
+        value.as_str()
+    }
+}
+
+impl Display for GitUrl {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+const ERR_MSG: &str = "a URL compatible with Git";
+
+impl FromStr for GitUrl {
+    type Err = &'static str;
+
+    fn from_str(v: &str) -> Result<Self, Self::Err> {
+        let ok = Ok(GitUrl(v.to_owned()));
+
+        if let Some(rest) = v.strip_prefix("https://") {
+            if let Some((domain, other)) = rest.split_once('/') {
+                if domain.is_empty() {
+                    Err("domain is empty")?
+                }
+                if other.is_empty() {
+                    Err("part after domain is empty")?
+                }
+            } else {
+                Err("expect a '/' between domain and location part")?
+            }
+            return ok;
+        }
+
+        // XX is this correct, was it git:// ? All the rest the same or ?
+        if let Some(rest) = v.strip_prefix("git://") {
+            if let Some((domain, other)) = rest.split_once('/') {
+                if domain.is_empty() {
+                    Err("domain is empty")?
+                }
+                if other.is_empty() {
+                    Err("part after domain is empty")?
+                }
+            } else {
+                Err("expect a '/' between domain and location part")?
+            }
+            return ok;
+        }
+
+        if let Some(rest) = v.strip_prefix("file://") {
+            if rest.is_empty() {
+                Err("empty file path given")?
+            }
+            return ok;
+        }
+
+        if v.starts_with("/") || v.starts_with("../") {
+            // OK?
+            return ok;
+        }
+
+        if let Some((user, rest)) = v.split_once('@') {
+            if user.is_empty() {
+                Err("user is empty")?
+            }
+            if let Some((domain, _path)) = rest.split_once(':') {
+                if domain.is_empty() {
+                    Err("domain is empty")?
+                }
+                // I guess path *can* be empty, if the home dir is the repo.
+            } else {
+                Err("missing ':' in ssh based Git URL")?
+            }
+            return ok;
+        }
+
+        Err("no match for any kind of Git url known to this code")
+    }
+}
+
+struct GitUrlVisitor;
+impl<'de> Visitor<'de> for GitUrlVisitor {
+    type Value = GitUrl;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str(ERR_MSG)
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        v.parse().map_err(E::custom)
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for GitUrl {
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        deserializer.deserialize_str(GitUrlVisitor)
+    }
+}
--- evobench-evaluator/src/serde/mod.rs
+++ evobench-evaluator/src/serde/mod.rs
@@ -1,4 +1,5 @@
 pub mod date_and_time;
+pub mod git_url;
 pub mod key_val;
 pub mod map;
 pub mod paths;
