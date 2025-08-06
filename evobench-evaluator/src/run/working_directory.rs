bd1ff7de872e9b2ccf43191dcc4358532bff9dab
Christian Jaeger <ch@christianjaeger.ch>
2025-08-06 13:15:03 +0200

    run/working_directory*.rs: extend and save working directory status
    - run/working_directory.rs:
    
        - `Status`: drop `Built`, add `Processing`, `Error`, `Finished`
    
        - add struct WorkingDirectoryStatus, that also contains `num_runs` and `creation_timestamp`
        - save .status file
    
    - run/working_directory_pool.rs:
    
        - rename `entries` to `all_entries`, `len()` to `active_len()`,
    
        - add `active_entries()` to filter out inactive entries, and in
          once case check and refuse to work with an inactive entry.
    
        - rename `set_processing_error()` to `save_processing_error()`,
          remove the dir rename
    
        - call working_directory.XXX statius
    
        - (give try_get_best_working_directory_for a more appropriate name)

--- evobench-evaluator/src/run/working_directory.rs
+++ evobench-evaluator/src/run/working_directory.rs
@@ -6,26 +6,53 @@ use std::{
     time::SystemTime,
 };
 
-use anyhow::{bail, Result};
-use run_git::git::{git_clone, GitResetMode, GitWorkingDir};
+use anyhow::{anyhow, bail, Result};
+use run_git::{
+    git::{git_clone, GitResetMode, GitWorkingDir},
+    path_util::add_extension,
+};
+use serde::{Deserialize, Serialize};
 
-use crate::{ctx, git::GitHash, info, serde::git_url::GitUrl};
+use crate::{
+    config_file::{load_ron_file, ron_to_file_pretty},
+    ctx,
+    git::GitHash,
+    info,
+    serde::{date_and_time::DateTimeWithOffset, git_url::GitUrl},
+};
 
 const NO_OPTIONS: &[&str] = &[];
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, Copy, PartialEq, Eq)]
 pub enum Status {
+    /// Only checked out for that commit
     CheckedOut,
-    Built,
-    Benchmarked,
+    /// Currently running in process_working_directory
+    Processing,
+    /// Project benchmarking gave error for that commit (means working dir is set aside)
+    Error,
+    /// Project benchmarking ran through for that commit
+    Finished,
 }
 
 impl Status {
-    pub fn score(self) -> u32 {
+    /// How well the dir is usable for a given commit id
+    fn score(self) -> u32 {
         match self {
             Status::CheckedOut => 1,
-            Status::Built => 2,
-            Status::Benchmarked => 3,
+            Status::Processing => 2,
+            Status::Error => 3,
+            Status::Finished => 4,
+        }
+    }
+
+    /// Being set aside means, can't be allocated for jobs
+    pub fn is_set_aside(self) -> bool {
+        match self {
+            Status::CheckedOut => false,
+            Status::Processing => false,
+            Status::Error => true,
+            Status::Finished => false,
         }
     }
 }
@@ -42,49 +69,123 @@ impl Ord for Status {
     }
 }
 
+/// Stored in `$n.status` files for each working directory
+#[derive(Debug, Serialize, Deserialize)]
+pub struct WorkingDirectoryStatus {
+    pub creation_timestamp: DateTimeWithOffset,
+    pub num_runs: usize,
+    pub status: Status,
+}
+
+impl WorkingDirectoryStatus {
+    fn new() -> Self {
+        Self {
+            creation_timestamp: DateTimeWithOffset::now(),
+            num_runs: 0,
+            status: Status::CheckedOut,
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct WorkingDirectory {
     pub git_working_dir: GitWorkingDir,
     pub commit: GitHash,
-    pub status: Status,
-    /// last use time: mtime of the folder, which is touched on every
-    /// use, too
+    pub working_directory_status: WorkingDirectoryStatus,
+    /// last use time: mtime of the .status file
     pub mtime: SystemTime,
 }
 
 impl WorkingDirectory {
+    fn status_path_from_working_dir_path(path: &Path) -> Result<PathBuf> {
+        add_extension(&path, "status")
+            .ok_or_else(|| anyhow!("can't add extension to path {path:?}"))
+    }
+
+    fn status_path(&self) -> Result<PathBuf> {
+        Self::status_path_from_working_dir_path(self.git_working_dir.working_dir_path_ref())
+    }
+
     pub fn open(path: PathBuf) -> Result<Self> {
         // let quiet = false;
+
+        let status_path = Self::status_path_from_working_dir_path(&path)?;
+        let (mtime, status);
+        match status_path.metadata() {
+            Ok(metadata) => {
+                mtime = metadata.modified()?;
+                status = load_ron_file(&status_path)?;
+            }
+            Err(e) => {
+                match e.kind() {
+                    std::io::ErrorKind::NotFound => {
+                        info!(
+                            "note: missing working directory status file {status_path:?}, \
+                             creating from defaults"
+                        );
+                        mtime = SystemTime::now();
+                        status = WorkingDirectoryStatus::new();
+                    }
+                    _ => {
+                        return Err(e).map_err(ctx!(
+                            "checking working directory status file path {status_path:?}"
+                        ))
+                    }
+                };
+            }
+        }
+
         let git_working_dir = GitWorkingDir::from(path);
-        let mtime = {
+        {
             let path = git_working_dir.working_dir_path_ref();
             std::fs::metadata(path)
                 .map_err(ctx!("WorkingDirectory::open({path:?})"))?
                 .modified()?
         };
         let commit: GitHash = git_working_dir.get_head_commit_id()?.parse()?;
-        let status = Status::CheckedOut;
         Ok(Self {
             git_working_dir,
             commit,
-            status,
+            working_directory_status: status,
             mtime,
         })
+    }
+
+    /// (Re-)save `$n.status` file (pool is locked (races?, when is
+    /// the lock taken?--XXX OH, actually there is no lock!), nobody
+    /// can change such a file, thus we do not have to re-check if it
+    /// was changed on disk)
+    fn save_status(&self) -> Result<()> {
+        let path = self.status_path()?;
+        ron_to_file_pretty(&self.working_directory_status, &path, None)
+    }
+
+    /// Give `increment_run_count == true` if you set it to
+    /// Status::Processing` (XX do this automatically? But have to
+    /// check if it is a change?)
+    pub fn change_status(&mut self, to_status: Status, increment_run_count: bool) -> Result<()> {
+        self.working_directory_status.status = to_status;
+        if increment_run_count {
+            self.working_directory_status.num_runs += 1;
+        }
+        self.save_status()
     }
 
     pub fn clone_repo(base_dir: &Path, dir_file_name: &str, url: &GitUrl) -> Result<Self> {
         let quiet = false;
         let git_working_dir = git_clone(&base_dir, [], url.as_str(), dir_file_name, quiet)?;
         let commit: GitHash = git_working_dir.get_head_commit_id()?.parse()?;
-        let status = Status::CheckedOut;
+        let status = WorkingDirectoryStatus::new();
         let mtime = std::fs::metadata(git_working_dir.working_dir_path_ref())?.modified()?;
         info!("clone_repo({base_dir:?}, {dir_file_name:?}, {url}) succeeded");
-        Ok(Self {
+        let slf = Self {
             git_working_dir,
             commit,
-            status,
+            working_directory_status: status,
             mtime,
-        })
+        };
+        slf.save_status()?;
+        Ok(slf)
     }
 
     /// Checks and is a no-op if already on the commit.
@@ -120,7 +221,8 @@ impl WorkingDirectory {
                 self.git_working_dir.working_dir_path_ref()
             );
             self.commit = commit;
-            self.status = Status::CheckedOut;
+            self.working_directory_status.status = Status::CheckedOut;
+            self.save_status()?;
             Ok(())
         }
     }
--- evobench-evaluator/src/run/working_directory_pool.rs
+++ evobench-evaluator/src/run/working_directory_pool.rs
@@ -6,7 +6,7 @@
 
 use std::{collections::BTreeMap, num::NonZeroU8, path::PathBuf, str::FromStr, sync::Arc, u64};
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, bail, Result};
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -19,7 +19,10 @@ use crate::{
     serde::{date_and_time::DateTimeWithOffset, git_url::GitUrl},
 };
 
-use super::{run_queues::RunQueuesData, working_directory::WorkingDirectory};
+use super::{
+    run_queues::RunQueuesData,
+    working_directory::{Status, WorkingDirectory},
+};
 
 // clap::Args?
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
@@ -111,7 +114,9 @@ pub struct WorkingDirectoryPool {
     // Actual basedir used (opts only has an Option!)
     base_dir: WorkingDirectoryPoolBaseDir,
     next_id: u64,
-    entries: BTreeMap<WorkingDirectoryId, WorkingDirectory>,
+    /// Contains working dirs with Status::Error, too, must be ignored
+    /// when picking a dir!
+    all_entries: BTreeMap<WorkingDirectoryId, WorkingDirectory>,
     /// Only one process may use this pool at the same time
     _lock: StandaloneExclusiveFileLock,
 }
@@ -139,7 +144,7 @@ impl WorkingDirectoryPool {
 
         let mut next_id: u64 = 0;
 
-        let entries: BTreeMap<WorkingDirectoryId, WorkingDirectory> =
+        let all_entries: BTreeMap<WorkingDirectoryId, WorkingDirectory> =
             std::fs::read_dir(base_dir.path())
                 .map_err(ctx!("opening working pool directory {:?}", base_dir.path()))?
                 .map(
@@ -194,13 +199,13 @@ impl WorkingDirectoryPool {
             base_dir,
             _lock: lock,
             next_id,
-            entries,
+            all_entries,
         };
 
         info!(
             "opened directory pool {:?} with next_id {next_id}, len {}/{}",
             slf.base_dir,
-            slf.len(),
+            slf.active_len(),
             slf.capacity()
         );
 
@@ -221,18 +226,26 @@ impl WorkingDirectoryPool {
         &self.remote_repository_url
     }
 
-    pub fn len(&self) -> usize {
-        self.entries.len()
+    pub fn active_entries(&self) -> impl Iterator<Item = (&WorkingDirectoryId, &WorkingDirectory)> {
+        self.all_entries
+            .iter()
+            .filter(|(_, wd)| !wd.working_directory_status.status.is_set_aside())
+    }
+
+    /// The number of entries that are not of Status::Error
+    pub fn active_len(&self) -> usize {
+        // Could optimize to not scan, but would want an abstraction
+        // around the entries for that, don't do now.
+        self.active_entries().count()
     }
 
     /// Always gets a working directory, but doesn't check for any
     /// best fit. If none was cloned yet, that is done now.
     pub fn get_first(&mut self) -> Result<WorkingDirectoryId> {
-        if let Some((key, _)) = self.entries.first_key_value() {
-            Ok(*key)
-        } else {
-            self.get_new()
+        if let Some((key, _)) = self.active_entries().next() {
+            return Ok(*key);
         }
+        self.get_new()
     }
 
     /// This is *not* public as it is not checking whether there is
@@ -244,33 +257,29 @@ impl WorkingDirectoryPool {
             &id.to_directory_file_name(),
             self.git_url(),
         )?;
-        self.entries.insert(id, dir);
+        self.all_entries.insert(id, dir);
         Ok(id)
     }
 
-    pub fn set_processing_error(
+    /// Save a processing error (not doing that to the status since
+    /// that would get overwritten when changing it back to an active
+    /// status). This method does *not* change the status of the
+    /// working directory, that must be done separately.
+    fn save_processing_error(
         &mut self,
         id: WorkingDirectoryId,
         processing_error: ProcessingError,
         timestamp: &DateTimeWithOffset,
     ) -> Result<()> {
-        let old_dir_path = self.base_dir().path().append(id.to_directory_file_name());
-        let new_dir_path = self.base_dir().path().append(format!(
-            "{}.dir_at_{timestamp}",
-            id.to_directory_file_name()
-        ));
         let error_file_path = self.base_dir().path().append(format!(
             "{}.error_at_{timestamp}",
             id.to_directory_file_name()
         ));
         let processing_error_string = serde_yml::to_string(&processing_error)?;
-        std::fs::rename(&old_dir_path, &new_dir_path)
-            .map_err(ctx!("renaming {old_dir_path:?} to {new_dir_path:?}"))?;
         std::fs::write(&error_file_path, &processing_error_string)
             .map_err(ctx!("writing to {error_file_path:?}"))?;
-        self.entries.remove(&id);
 
-        info!("set processing error on {id:?}");
+        info!("saved processing error to {error_file_path:?}");
 
         Ok(())
     }
@@ -293,12 +302,18 @@ impl WorkingDirectoryPool {
         self.set_current_working_directory(working_directory_id)?;
 
         let wd = self
-            .entries
+            .all_entries
             .get_mut(&working_directory_id)
             // Can't just .expect here because the use cases seem too
             // complex (concurrency means that a working directory
             // very well might disappear), thus:
             .ok_or_else(|| anyhow!("working directory id must still exist"))?;
+
+        if wd.working_directory_status.status.is_set_aside() {
+            bail!("working directory {working_directory_id:?} is set aside (error state)")
+        }
+
+        wd.change_status(Status::Processing, true)?;
 
         info!(
             "process_working_directory {working_directory_id:?} \
@@ -307,6 +322,8 @@ impl WorkingDirectoryPool {
 
         match action(wd) {
             Ok(v) => {
+                wd.change_status(Status::Finished, false)?;
+
                 info!(
                     "process_working_directory {working_directory_id:?} \
                      ({context}, {benchmarking_job_parameters:?}) succeeded."
@@ -315,6 +332,8 @@ impl WorkingDirectoryPool {
                 Ok(v)
             }
             Err(error) => {
+                wd.change_status(Status::Error, false)?;
+
                 info!(
                     // Do not show error as it might be large; XX
                     // which is a mis-feature!
@@ -323,7 +342,7 @@ impl WorkingDirectoryPool {
                 );
 
                 let err = format!("{error:#?}");
-                self.set_processing_error(
+                self.save_processing_error(
                     working_directory_id,
                     ProcessingError {
                         benchmarking_job_parameters: benchmarking_job_parameters.cloned(),
@@ -348,7 +367,7 @@ impl WorkingDirectoryPool {
     /// commit, and if possible already built or even tested for
     /// it. Returns its id so that the right kind of fresh borrow can
     /// be done.
-    fn try_get_best_working_directory_for(
+    fn try_get_fitting_working_directory_for(
         &self,
         run_parameters: &RunParameters,
         run_queues_data: &RunQueuesData,
@@ -360,12 +379,11 @@ impl WorkingDirectoryPool {
 
         // Find one with the same commit
         if let Some((id, _dir)) = self
-            .entries
-            .iter()
+            .active_entries()
             .filter(|(_, dir)| dir.commit == *commit)
             // Prefer one that proceeded further and is matching
             // closely: todo: store parameters for dir.
-            .max_by_key(|(_, dir)| dir.status)
+            .max_by_key(|(_, dir)| dir.working_directory_status.status)
         {
             info!("try_get_best_working_directory_for: found by commit {commit}");
             return Some(*id);
@@ -374,10 +392,9 @@ impl WorkingDirectoryPool {
         // Find one that is *not* used by other jobs in the pipeline (i.e. obsolete),
         // and todo: similar parameters
         if let Some((id, _dir)) = self
-            .entries
-            .iter()
+            .active_entries()
             .filter(|(_, dir)| !run_queues_data.have_entry_with_commit_id(&dir.commit))
-            .max_by_key(|(_, dir)| dir.status)
+            .max_by_key(|(_, dir)| dir.working_directory_status.status)
         {
             info!("try_get_best_working_directory_for: found as obsolete");
             return Some(*id);
@@ -398,11 +415,13 @@ impl WorkingDirectoryPool {
         run_parameters: &RunParameters,
         run_queues_data: &RunQueuesData,
     ) -> Result<WorkingDirectoryId> {
-        if let Some(id) = self.try_get_best_working_directory_for(run_parameters, run_queues_data) {
+        if let Some(id) =
+            self.try_get_fitting_working_directory_for(run_parameters, run_queues_data)
+        {
             info!("get_a_working_directory_for -> good old {id:?}");
             Ok(id)
         } else {
-            if self.len() < self.capacity() {
+            if self.active_len() < self.capacity() {
                 // allocate a new one
                 let id = self.get_new()?;
                 info!("get_a_working_directory_for -> new {id:?}");
@@ -410,8 +429,7 @@ impl WorkingDirectoryPool {
             } else {
                 // get the least-recently used one
                 let id = self
-                    .entries
-                    .iter()
+                    .active_entries()
                     .min_by_key(|(_, entry)| entry.mtime)
                     .expect("capacity is guaranteed >= 1")
                     .0
