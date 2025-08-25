60d2d3f433b3e7f535e29207735c9e1b98889bb2
Christian Jaeger <ch@christianjaeger.ch>
2025-08-25 15:21:13 +0200

    (evobench-probes) examples: add scope around key-value output
    To avoid the latter 'piling up' (arguably a design bug to be fixed).

--- evobench-probes/examples/sum_of_fibs.cpp
+++ evobench-probes/examples/sum_of_fibs.cpp
@@ -13,31 +13,32 @@ long long fib(long long n) {
 }
 
 long long sum_of_fibs(long long n) {
-   {
-      char buf[30];
-      snprintf(buf, 30, "%lld", n);
-      EVOBENCH_KEY_VALUE("sum_of_fibs n", buf);
-   }
-
-   EVOBENCH_SCOPE("main", "sum_of_fibs");
-   long long z = 0;
-   for (long long i = 0; i < n; i++) {
-      EVOBENCH_SCOPE("main", "fib");
-      struct timespec req = {
-         .tv_sec = 0,
-         .tv_nsec = 10000000,
-      };
-      nanosleep(&req, NULL);
-      z += fib(i);
-   }
-   return z;
+    EVOBENCH_SCOPE("sum_of_fibs", "all");
+    {
+	char buf[30];
+	snprintf(buf, 30, "%lld", n);
+	EVOBENCH_KEY_VALUE("sum_of_fibs n", buf);
+    }
+    
+    EVOBENCH_SCOPE("sum_of_fibs", "body");
+    long long z = 0;
+    for (long long i = 0; i < n; i++) {
+	EVOBENCH_SCOPE("main", "fib");
+	struct timespec req = {
+	    .tv_sec = 0,
+	    .tv_nsec = 10000000,
+	};
+	nanosleep(&req, NULL);
+	z += fib(i);
+    }
+    return z;
 }
 
 int main() {
-   EVOBENCH_SCOPE("main", "main");
-   sleep(1);
-   for (long long i = 0; i < 40; i++) {
-      std::cout << "sum_of_fibs(" << i << ") = " << sum_of_fibs(i) << "\n";
-   }
-   return 0;
+    EVOBENCH_SCOPE("main", "main");
+    sleep(1);
+    for (long long i = 0; i < 40; i++) {
+	std::cout << "sum_of_fibs(" << i << ") = " << sum_of_fibs(i) << "\n";
+    }
+    return 0;
 }
