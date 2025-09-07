0b24d8b08dba202404b13b28d2f1c22ee17d3ebd
Christian Jaeger <ch@christianjaeger.ch>
2025-09-07 13:48:13 +0200

    utillib/type_name_short.rs: add `fn short_name`

--- evobench-evaluator/src/utillib/type_name_short.rs
+++ evobench-evaluator/src/utillib/type_name_short.rs
@@ -1,8 +1,6 @@
 use std::any::type_name;
 
-/// `type_name` without the namespace
-pub fn type_name_short<T>() -> &'static str {
-    let s = type_name::<T>();
+pub fn short_name(s: &str) -> &str {
     let mut prev_i = None;
     for (i, c) in s.char_indices().rev() {
         if c == ':' {
@@ -13,6 +11,11 @@ pub fn type_name_short<T>() -> &'static str {
     // Fall back to the whole thing if the name at the end is 0
     // characters long--is this even possible even via quotation?
     &s[prev_i.unwrap_or(0)..]
+}
+
+/// `type_name` without the namespace
+pub fn type_name_short<T>() -> &'static str {
+    short_name(type_name::<T>())
 }
 
 #[cfg(test)]
