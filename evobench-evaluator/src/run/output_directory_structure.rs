f562ba8a7b8ff156f6368e75a73a9214e9098ffa
Christian Jaeger <ch@christianjaeger.ch>
2025-09-08 18:44:10 +0200

    run/output_directory_structure.rs: add parse() methods
    In particular, need the `target_name` method to know what
    configuration to use for a path in the upcoming post-processing
    utility.

--- evobench-evaluator/src/run/output_directory_structure.rs
+++ evobench-evaluator/src/run/output_directory_structure.rs
@@ -1,21 +1,24 @@
 //! The directory structure for output files.
 
 use std::{
+    collections::BTreeMap,
     fmt::Display,
     path::{Path, PathBuf},
     str::FromStr,
 };
 
 use anyhow::{anyhow, bail, Result};
+use kstring::KString;
 use run_git::path_util::AppendToPath;
 
 use crate::{
     ctx,
     git::GitHash,
     key::RunParameters,
+    run::run_job::AllowableCustomEnvVar,
     serde::{
-        date_and_time::DateTimeWithOffset, proper_dirname::ProperDirname,
-        proper_filename::ProperFilename,
+        allowed_env_var::AllowedEnvVar, date_and_time::DateTimeWithOffset,
+        proper_dirname::ProperDirname, proper_filename::ProperFilename,
     },
     utillib::type_name_short::type_name_short,
 };
@@ -96,6 +99,52 @@ impl CustomParametersDir {
         &self.0
     }
 
+    /// Returns `(target_name, custom_env_vars)`. This returns custom
+    /// env vars that are *not* checked against the config; they are
+    /// the raw values, but they still do follow the requirements for
+    /// env var names. Similarly, `target_name` is not checked against
+    /// anything (other than being a directory name).
+    pub fn parse(
+        &self,
+    ) -> Result<(
+        ProperDirname,
+        BTreeMap<AllowedEnvVar<AllowableCustomEnvVar>, KString>,
+    )> {
+        let mut path = self.path();
+        let mut params = BTreeMap::new();
+        loop {
+            if let Some(dir_name) = path.file_name() {
+                let dir_name_str = dir_name.to_str().ok_or_else(|| {
+                    anyhow!(
+                        "directory segment can't be decoded as string: {:?} in {:?}",
+                        dir_name.to_string_lossy().as_ref(),
+                        self.path()
+                    )
+                })?;
+                if let Some((var_name, val)) = dir_name_str.split_once('=') {
+                    let key = AllowedEnvVar::from_str(var_name)?;
+                    let val = KString::from_ref(val);
+                    params.insert(key, val);
+
+                    if let Some(parent) = path.parent() {
+                        path = parent;
+                    } else {
+                        bail!(
+                            "parsing {} {:?}: ran out of parent segments",
+                            type_name_short::<Self>(),
+                            self.path()
+                        );
+                    }
+                } else {
+                    let target_name = ProperDirname::from_str(dir_name_str).map_err(|msg| {
+                        anyhow!("not a proper directory name: {dir_name_str:?}: {msg}")
+                    })?;
+                    return Ok((target_name, params));
+                }
+            }
+        }
+    }
+
     pub fn key_dirs(&self) -> Result<Vec<KeyDir>> {
         typed_dir_listing_of_dirs(self.path())
     }
@@ -118,6 +167,30 @@ impl KeyDir {
     ) -> Self {
         KeyDir::try_from(run_parameters.extend_path(output_base_dir.append(target_name.as_str())))
             .expect("self-created paths follow the spec")
+    }
+
+    pub fn parse(
+        &self,
+    ) -> Result<(
+        ProperDirname,
+        BTreeMap<AllowedEnvVar<AllowableCustomEnvVar>, KString>,
+        GitHash,
+    )> {
+        let dir_name = self
+            .path()
+            .file_name()
+            .expect("filename guaranteed by construction");
+        let dir_name_str = dir_name
+            .to_str()
+            .expect("guaranteed parseable by construction");
+        let commit = GitHash::from_str(dir_name_str)?;
+        let parent_path = self
+            .path()
+            .parent()
+            .expect("parent guaranteed by construction");
+        let params_dir = CustomParametersDir(parent_path.to_owned());
+        let (target_name, params) = params_dir.parse()?;
+        Ok((target_name, params, commit))
     }
 
     pub fn path(&self) -> &Path {
@@ -143,6 +216,31 @@ impl TryFrom<PathBuf> for RunDir {
 }
 
 impl RunDir {
+    pub fn parse(
+        &self,
+    ) -> Result<(
+        ProperDirname,
+        BTreeMap<AllowedEnvVar<AllowableCustomEnvVar>, KString>,
+        GitHash,
+        DateTimeWithOffset,
+    )> {
+        let dir_name = self
+            .path()
+            .file_name()
+            .expect("filename guaranteed by construction");
+        let dir_name_str = dir_name
+            .to_str()
+            .expect("guaranteed parseable by construction");
+        let timestamp = DateTimeWithOffset::from_str(dir_name_str)?;
+        let parent_path = self
+            .path()
+            .parent()
+            .expect("parent guaranteed by construction");
+        let key_dir = KeyDir(parent_path.to_owned());
+        let (target_name, params, commit) = key_dir.parse()?;
+        Ok((target_name, params, commit, timestamp))
+    }
+
     pub fn path(&self) -> &Path {
         &self.0
     }
@@ -163,6 +261,10 @@ impl RunDir {
     /// target application while running this benchmark.
     pub fn standard_log_path(&self) -> PathBuf {
         self.path().append("standard.log.zstd")
+    }
+
+    pub fn target_name(&self) -> Result<ProperDirname> {
+        Ok(self.parse()?.0)
     }
 
     /// Files below a RunDir are normal files (no special type, at
