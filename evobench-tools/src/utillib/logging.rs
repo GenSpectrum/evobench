6b777dbf00f6ab3a7ea4d973214ffd6be4431877
Christian Jaeger <ch@christianjaeger.ch>
2026-01-12 13:21:50 +0100

    utillib/logging.rs: use Ordering::SeqCst for stores in set_log_level
    For consistency with the comments on `LOCAL_TIME`.

--- evobench-tools/src/utillib/logging.rs
+++ evobench-tools/src/utillib/logging.rs
@@ -162,7 +162,7 @@ impl Ord for LogLevel {
 pub static LOGLEVEL: AtomicU8 = AtomicU8::new(1);
 
 pub fn set_log_level(val: LogLevel) {
-    LOGLEVEL.store(val.level(), Ordering::Relaxed);
+    LOGLEVEL.store(val.level(), Ordering::SeqCst);
 }
 
 #[inline]
