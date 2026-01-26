c56dc44f300db4edc330a128f39c99b7c5843dab
Christian Jaeger <ch@christianjaeger.ch>
2026-01-26 17:00:35 +0100

    git_ext.rs: add `fetch_references`

--- evobench-tools/src/git_ext.rs
+++ evobench-tools/src/git_ext.rs
@@ -3,11 +3,18 @@
 use anyhow::{Result, bail};
 use run_git::git::GitWorkingDir;
 
-use crate::serde::git_url::GitUrl;
+use crate::serde::{git_reference::GitReference, git_url::GitUrl};
 
 pub trait MoreGitWorkingDir {
     fn get_url(&self, remote_name: &str) -> Result<String>;
     fn set_url(&self, remote_name: &str, url: &GitUrl) -> Result<()>;
+    fn fetch_references<R: AsRef<GitReference>, Rs: AsRef<[R]>>(
+        &self,
+        remote_name: &str,
+        tags: bool,
+        references: Rs,
+        quiet: bool,
+    ) -> Result<()>;
 }
 
 impl MoreGitWorkingDir for GitWorkingDir {
@@ -24,5 +31,33 @@ impl MoreGitWorkingDir for GitWorkingDir {
                 url.as_str()
             )
         }
+    }
+
+    /// Fetch branches (XXX does it do that?), optionally tags, and
+    /// optional explicit commit ids (generally references?)
+    fn fetch_references<R: AsRef<GitReference>, Rs: AsRef<[R]>>(
+        &self,
+        remote_name: &str,
+        fetch_all_tags: bool,
+        references: Rs,
+        quiet: bool,
+    ) -> Result<()> {
+        let mut args = vec!["fetch", remote_name];
+        if fetch_all_tags {
+            args.push("--tags");
+        }
+        let rs = references.as_ref();
+        for r in rs {
+            let reference = r.as_ref();
+            args.push(reference.as_ref());
+        }
+
+        // IIRC Git usually returns another exit code than 1 for
+        // actual errors, right? But to be sure:
+        if !self.git(&args, quiet)? {
+            bail!("git {args:?} was not successful");
+        }
+
+        Ok(())
     }
 }
