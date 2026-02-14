9dab58f7b6c4f91dfbc1633a890f986c004edd2c
Christian Jaeger <ch@christianjaeger.ch>
2026-02-14 22:11:51 +0100

    evaluator/data/log_data_and_tree.rs: add benchmarking output (temporary?)

--- evobench-tools/src/evaluator/data/log_data_and_tree.rs
+++ evobench-tools/src/evaluator/data/log_data_and_tree.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{path::Path, time::SystemTime};
 
 use anyhow::Result;
 use ouroboros::self_referencing;
@@ -16,9 +16,23 @@ pub struct LogDataAndTree {
 impl LogDataAndTree {
     /// `LogData::read_file` combined with `LogDataTree::from_logdata`
     pub fn read_file(path: &Path) -> Result<Self> {
+        let t0 = SystemTime::now();
         let log_data = LogData::read_file(path)?;
 
-        LogDataAndTree::try_new(log_data, |log_data| LogDataTree::from_logdata(log_data))
+        let t1 = SystemTime::now();
+        let r = LogDataAndTree::try_new(log_data, |log_data| LogDataTree::from_logdata(log_data));
+        let t2 = SystemTime::now();
+
+        eprintln!(
+            "t LogData::read_file: {} s",
+            t1.duration_since(t0)?.as_secs_f64()
+        );
+        eprintln!(
+            "t LogDataAndTree::try_new: {} s",
+            t2.duration_since(t1)?.as_secs_f64()
+        );
+
+        r
     }
 
     pub fn log_data(&self) -> &LogData {
